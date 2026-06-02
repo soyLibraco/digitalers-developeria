1e2205f8b5d34ef30f4e9bef2081777f6284ce6e
soyLibraco <soylibraco@gmail.com>
2026-06-02 19:42:27 -0300

    Reorganización de archivos y carpetas

--- 4_input.py
+++
@@ -1,7 +0,0 @@
-# Función input()
-
-nombre = input("Dime tu nombre: ")
-
-print("Hola " + nombre)
-print("Hola", nombre)
-print(f"Hola {nombre}")
--- 6_conversiones.py
+++
@@ -1,8 +0,0 @@
-numero_1 = int(input("Ingresa un número: "))
-numero_2 = int(input("Ingresa otro número: "))
-
-# suma = int(numero_1) + int(numero_2)
-suma = numero_1 + numero_2
-
-print(f"{numero_1} + {numero_2} = {suma}")
-
