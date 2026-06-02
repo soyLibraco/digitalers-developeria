0caf952df24fbb392647b812e03721022f14ff57
soyLibraco <soylibraco@gmail.com>
2026-06-02 18:58:11 -0300

    Primer clase Digitalers

---
+++ 6_conversiones.py
@@ -0,0 +1,8 @@
+numero_1 = int(input("Ingresa un número: "))
+numero_2 = int(input("Ingresa otro número: "))
+
+# suma = int(numero_1) + int(numero_2)
+suma = numero_1 + numero_2
+
+print(f"{numero_1} + {numero_2} = {suma}")
+
